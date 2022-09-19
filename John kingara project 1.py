ee3e3ebc92cf903f56ef4edd7cfb8504e3f17a07
J1kingara <113391348+J1kingara@users.noreply.github.com>
2022-09-19 19:41:12 -0400

    .

---
+++ John kingara project 1.py
@@ -0,0 +1,10 @@
+# project 1
+
+year_born = 2002
+name = " john kingara "
+current_year = 2022
+
+age = current_year - year_born
+
+print (f"my name is" {name} " and this year i will be" {age} "years old" )
+
